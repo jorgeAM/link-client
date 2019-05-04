c09b292027d97acdc9b66448722ea97c7c64df60
Jorge L <jorge.alfmur@gmail.com>
2019-05-04 15:11:33 -0500

    get links from graphQL server

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
 import Link from './Link';
+
+const FEED_QUERY = gql`
+{
+    feed {
+        id
+        url
+        description
+    }
+}
+`
 
 class LinkList extends Component {
     render() {
         return (
-            <div>
-                {this.props.links.map(link => <Link key={link.id} link={link} />)}
-            </div>
+            <Query query={FEED_QUERY}>
+            {({ loading, error, data}) => {
+                if (loading) return <p>Loading ...</p>
+                if (error) return <p>Something get wrong :c</p>
+                return data.feed.map(link => <Link key={link.id} link={link} />)
+            }}
+            </Query>
         )
     }
 }
